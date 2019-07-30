59c1ec6e8077a1324d98bfe7599487a1dad32d3a
Christopher Katsaras <chriskatsaras5@gmail.com>
2019-07-29 20:15:23 -0400

    Added type definition file

---
+++ src/index.d.ts
@@ -0,0 +1,38 @@
+/// <reference types="react" />
+import { ComponentType, SVGAttributes } from 'react';
+
+interface Props extends SVGAttributes<SVGElement> {
+  size?: string | number;
+}
+
+type Icon = ComponentType<Props>;
+export const ATL: Icon;
+export const BKN: Icon;
+export const BOS: Icon;
+export const CHA: Icon;
+export const CHI: Icon;
+export const CLE: Icon;
+export const DAL: Icon;
+export const DEN: Icon;
+export const DET: Icon;
+export const GSW: Icon;
+export const HOU: Icon;
+export const IND: Icon;
+export const LAC: Icon;
+export const LAL: Icon;
+export const MEM: Icon;
+export const MIA: Icon;
+export const MIL: Icon;
+export const MIN: Icon;
+export const NOP: Icon;
+export const NYK: Icon;
+export const OKC: Icon;
+export const ORL: Icon;
+export const PHI: Icon;
+export const PHX: Icon;
+export const POR: Icon;
+export const SAC: Icon;
+export const SAS: Icon;
+export const TOR: Icon;
+export const UTA: Icon;
+export const WAS: Icon;
