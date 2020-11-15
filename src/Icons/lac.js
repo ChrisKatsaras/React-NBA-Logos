93e0a8ff454be276b4ab7395c2ff98dbccd07bdb
Adam Sulima Dolina <adam.sulimadolina@gmail.com>
2020-11-15 22:30:16 +0100

    Edited IDs in logos files

--- src/Icons/lac.js
+++ src/Icons/lac.js
@@ -1,54 +1,61 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+"use strict";
 
-const LAC = props => {
-  const { size } = props;
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      xmlnsXlink="http://www.w3.org/1999/xlink"
-      width={size}
-      height={size}
-      viewBox="0 0 150 150"
-      fill="none"
-      fillRule="evenodd"
-    >
-      <defs>
-        <path id="A" d="M0 100h100V0H0z" />
-      </defs>
-      <g transform="translate(25 25)" fillRule="evenodd">
-        <path
-          d="M0 50C0 22.386 22.386 0 50 0s50 22.386 50 50-22.386 50-50 50S0 77.614 0 50"
-          fill="#fff"
-        />
-        <path
-          d="M84.363 76.412c-5.447-7.064-8.54-15.462-8.963-24.33h17.913c-.423 8.873-3.512 17.273-8.95 24.33m-32.28 16.9V80.89h-4.167v12.42c-11.284-.537-21.748-5.376-29.518-13.652 6.35-7.93 9.933-17.455 10.37-27.577v-4.167c-.44-10.12-4.02-19.647-10.37-27.577 7.77-8.277 18.235-13.116 29.518-13.652v12.3h4.167v-12.3c11.284.536 21.75 5.376 29.52 13.652-6.35 7.93-9.933 17.455-10.37 27.577v4.167c.44 10.12 4.02 19.647 10.37 27.577-7.77 8.277-18.236 13.115-29.52 13.652m-36.446-16.9a42.98 42.98 0 0 1-8.95-24.329H24.6c-.422 8.866-3.517 17.265-8.963 24.33m0-52.824c5.447 7.064 8.54 15.463 8.963 24.33H6.688a42.98 42.98 0 0 1 8.95-24.329m68.726 0a42.99 42.99 0 0 1 8.95 24.329H75.4c.422-8.865 3.516-17.264 8.963-24.33M50 2.462C23.788 2.462 2.462 23.788 2.462 50S23.788 97.538 50 97.538 97.538 76.213 97.538 50 76.213 2.462 50 2.462"
-          fill="#061922"
-        />
-        <path fill="#c8102e" d="M39.83 30.17v39.66h21.2v-5.453H45.5V30.17z" />
-        <path
-          d="M68.222 43.457V27.115c0-2.45-.986-3.448-3.403-3.448H35.976c-2.386 0-3.36 1-3.36 3.448v45.93c0 2.448.974 3.447 3.36 3.447H64.82c2.417 0 3.403-1 3.403-3.447v-16.36h-5.715v14.792H38.288V28.683h24.218v14.774h5.715z"
-          fill="#1d428a"
-        />
-        <mask id="B" fill="#fff">
-          <use xlinkHref="#A" />
-        </mask>
-        <path
-          d="M52.72 47.224h2.762V35.622H52.72v11.602zm8.29 15.618l.01-32.673H47.25l-.134 32.673h5.513l.09-10.186h2.803l-.04 10.186h5.528z"
-          fill="#c8102e"
-          mask="url(#B)"
-        />
-      </g>
-    </svg>
-  );
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _propTypes = _interopRequireDefault(require("prop-types"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var LAC = function LAC(props) {
+  var size = props.size;
+  return _react["default"].createElement("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    xmlnsXlink: "http://www.w3.org/1999/xlink",
+    width: size,
+    height: size,
+    viewBox: "0 0 150 150",
+    fill: "none",
+    fillRule: "evenodd"
+  }, _react["default"].createElement("defs", null, _react["default"].createElement("path", {
+    id: "A_LAC",
+    d: "M0 100h100V0H0z"
+  })), _react["default"].createElement("g", {
+    transform: "translate(25 25)",
+    fillRule: "evenodd"
+  }, _react["default"].createElement("path", {
+    d: "M0 50C0 22.386 22.386 0 50 0s50 22.386 50 50-22.386 50-50 50S0 77.614 0 50",
+    fill: "#fff"
+  }), _react["default"].createElement("path", {
+    d: "M84.363 76.412c-5.447-7.064-8.54-15.462-8.963-24.33h17.913c-.423 8.873-3.512 17.273-8.95 24.33m-32.28 16.9V80.89h-4.167v12.42c-11.284-.537-21.748-5.376-29.518-13.652 6.35-7.93 9.933-17.455 10.37-27.577v-4.167c-.44-10.12-4.02-19.647-10.37-27.577 7.77-8.277 18.235-13.116 29.518-13.652v12.3h4.167v-12.3c11.284.536 21.75 5.376 29.52 13.652-6.35 7.93-9.933 17.455-10.37 27.577v4.167c.44 10.12 4.02 19.647 10.37 27.577-7.77 8.277-18.236 13.115-29.52 13.652m-36.446-16.9a42.98 42.98 0 0 1-8.95-24.329H24.6c-.422 8.866-3.517 17.265-8.963 24.33m0-52.824c5.447 7.064 8.54 15.463 8.963 24.33H6.688a42.98 42.98 0 0 1 8.95-24.329m68.726 0a42.99 42.99 0 0 1 8.95 24.329H75.4c.422-8.865 3.516-17.264 8.963-24.33M50 2.462C23.788 2.462 2.462 23.788 2.462 50S23.788 97.538 50 97.538 97.538 76.213 97.538 50 76.213 2.462 50 2.462",
+    fill: "#061922"
+  }), _react["default"].createElement("path", {
+    fill: "#c8102e",
+    d: "M39.83 30.17v39.66h21.2v-5.453H45.5V30.17z"
+  }), _react["default"].createElement("path", {
+    d: "M68.222 43.457V27.115c0-2.45-.986-3.448-3.403-3.448H35.976c-2.386 0-3.36 1-3.36 3.448v45.93c0 2.448.974 3.447 3.36 3.447H64.82c2.417 0 3.403-1 3.403-3.447v-16.36h-5.715v14.792H38.288V28.683h24.218v14.774h5.715z",
+    fill: "#1d428a"
+  }), _react["default"].createElement("mask", {
+    id: "B_LAC",
+    fill: "#fff"
+  }, _react["default"].createElement("use", {
+    xlinkHref: "#A_LAC"
+  })), _react["default"].createElement("path", {
+    d: "M52.72 47.224h2.762V35.622H52.72v11.602zm8.29 15.618l.01-32.673H47.25l-.134 32.673h5.513l.09-10.186h2.803l-.04 10.186h5.528z",
+    fill: "#c8102e",
+    mask: "url(#B_LAC)"
+  })));
 };
 
 LAC.propTypes = {
-  size: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+  size: _propTypes["default"].oneOfType([_propTypes["default"].string, _propTypes["default"].number])
 };
-
 LAC.defaultProps = {
   size: '100'
 };
-
-export default LAC;
+var _default = LAC;
+exports["default"] = _default;
--- src/Icons/uta.js
+++ src/Icons/uta.js
@@ -1,75 +1,75 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+"use strict";
 
-const UTA = props => {
-  const { size } = props;
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      xmlnsXlink="http://www.w3.org/1999/xlink"
-      width={size}
-      height={size}
-      viewBox="0 0 150 150"
-      fill="none"
-      fillRule="evenodd"
-    >
-      <defs>
-        <path id="A" d="M120.346 80.052V.086H0v79.966h120.345z" />
-      </defs>
-      <g fill="none" fillRule="evenodd">
-        <g transform="translate(15 34.195)">
-          <mask id="B" fill="#fff">
-            <use xlinkHref="#A" />
-          </mask>
-          <path
-            d="M43.604 43.647L62.607.085h57.74l-10.462 23.993h-48.77L47.3 55.763v1.12c0 10.094-8.52 23.168-23.655 23.168C10.608 80.052 0 69.423 0 56.372c0-13.06 10.608-23.676 23.643-23.676 8.23 0 15.94 4.444 19.96 10.95"
-            fill="#fefefe"
-            mask="url(#B)"
-          />
-        </g>
-        <path
-          d="M16.415 91.623c.274 5.87 2.815 11.13 6.75 14.962 4.187-3.703 6.928-9.013 7.22-14.962h-13.97z"
-          fill="#0b223e"
-        />
-        <path
-          d="M32.346 91.623c-.292 6.45-3.212 12.223-7.706 16.27 3.582 2.915 8.094 4.748 13.035 4.963V91.623h-5.33z"
-          fill="#f9a11a"
-        />
-        <path
-          d="M16.407 89.672H30.4c-.25-6.018-2.994-11.396-7.226-15.12-3.97 3.864-6.52 9.192-6.758 15.12"
-          fill="#0b223e"
-        />
-        <path
-          d="M24.64 73.244c4.536 4.076 7.46 9.91 7.706 16.428h5.33v-21.4a22.09 22.09 0 0 0-13.035 4.972"
-          fill="#f9a11a"
-        />
-        <path
-          d="M60.883 91.623H46.917c.292 5.95 3.03 11.26 7.217 14.962 3.94-3.832 6.474-9.09 6.75-14.962"
-          fill="#0b223e"
-        />
-        <path
-          d="M44.96 91.623h-5.332v21.233c4.94-.215 9.448-2.05 13.038-4.963-4.494-4.047-7.417-9.82-7.706-16.27m7.705-18.38a22.11 22.11 0 0 0-13.038-4.972v21.4h5.326c.248-6.52 3.18-12.352 7.712-16.428"
-          fill="#2b5134"
-        />
-        <path
-          d="M78.527 35.69L58.755 81.038a22.43 22.43 0 0 0-4.619-6.483c-4.23 3.724-6.975 9.103-7.223 15.12H60.9l14.297-32.81h41.77l9.23-21.177h-47.66z"
-          fill="#0b223e"
-        />
-        <path
-          fill="#2b5134"
-          d="M123.964 56.864l9.233-21.177h-3.937l-9.23 21.177z"
-        />
-      </g>
-    </svg>
-  );
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _propTypes = _interopRequireDefault(require("prop-types"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var UTA = function UTA(props) {
+  var size = props.size;
+  return _react["default"].createElement("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    xmlnsXlink: "http://www.w3.org/1999/xlink",
+    width: size,
+    height: size,
+    viewBox: "0 0 150 150",
+    fill: "none",
+    fillRule: "evenodd"
+  }, _react["default"].createElement("defs", null, _react["default"].createElement("path", {
+    id: "A_UTA",
+    d: "M120.346 80.052V.086H0v79.966h120.345z"
+  })), _react["default"].createElement("g", {
+    fill: "none",
+    fillRule: "evenodd"
+  }, _react["default"].createElement("g", {
+    transform: "translate(15 34.195)"
+  }, _react["default"].createElement("mask", {
+    id: "B_UTA",
+    fill: "#fff"
+  }, _react["default"].createElement("use", {
+    xlinkHref: "#A_UTA"
+  })), _react["default"].createElement("path", {
+    d: "M43.604 43.647L62.607.085h57.74l-10.462 23.993h-48.77L47.3 55.763v1.12c0 10.094-8.52 23.168-23.655 23.168C10.608 80.052 0 69.423 0 56.372c0-13.06 10.608-23.676 23.643-23.676 8.23 0 15.94 4.444 19.96 10.95",
+    fill: "#fefefe",
+    mask: "url(#B_UTA)"
+  })), _react["default"].createElement("path", {
+    d: "M16.415 91.623c.274 5.87 2.815 11.13 6.75 14.962 4.187-3.703 6.928-9.013 7.22-14.962h-13.97z",
+    fill: "#0b223e"
+  }), _react["default"].createElement("path", {
+    d: "M32.346 91.623c-.292 6.45-3.212 12.223-7.706 16.27 3.582 2.915 8.094 4.748 13.035 4.963V91.623h-5.33z",
+    fill: "#f9a11a"
+  }), _react["default"].createElement("path", {
+    d: "M16.407 89.672H30.4c-.25-6.018-2.994-11.396-7.226-15.12-3.97 3.864-6.52 9.192-6.758 15.12",
+    fill: "#0b223e"
+  }), _react["default"].createElement("path", {
+    d: "M24.64 73.244c4.536 4.076 7.46 9.91 7.706 16.428h5.33v-21.4a22.09 22.09 0 0 0-13.035 4.972",
+    fill: "#f9a11a"
+  }), _react["default"].createElement("path", {
+    d: "M60.883 91.623H46.917c.292 5.95 3.03 11.26 7.217 14.962 3.94-3.832 6.474-9.09 6.75-14.962",
+    fill: "#0b223e"
+  }), _react["default"].createElement("path", {
+    d: "M44.96 91.623h-5.332v21.233c4.94-.215 9.448-2.05 13.038-4.963-4.494-4.047-7.417-9.82-7.706-16.27m7.705-18.38a22.11 22.11 0 0 0-13.038-4.972v21.4h5.326c.248-6.52 3.18-12.352 7.712-16.428",
+    fill: "#2b5134"
+  }), _react["default"].createElement("path", {
+    d: "M78.527 35.69L58.755 81.038a22.43 22.43 0 0 0-4.619-6.483c-4.23 3.724-6.975 9.103-7.223 15.12H60.9l14.297-32.81h41.77l9.23-21.177h-47.66z",
+    fill: "#0b223e"
+  }), _react["default"].createElement("path", {
+    fill: "#2b5134",
+    d: "M123.964 56.864l9.233-21.177h-3.937l-9.23 21.177z"
+  })));
 };
 
 UTA.propTypes = {
-  size: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+  size: _propTypes["default"].oneOfType([_propTypes["default"].string, _propTypes["default"].number])
 };
-
 UTA.defaultProps = {
   size: '100'
 };
-
-export default UTA;
+var _default = UTA;
+exports["default"] = _default;
